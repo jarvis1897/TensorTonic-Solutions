cbd77e9ba3ebd10a4c0731613d442caebff5fee9
Ritik Ranjan <42905278+jarvis1897@users.noreply.github.com>
2026-03-15 13:59:48 -0500

    Update solution for bernoulli-pmf

--- bernoulli-pmf/bernoulli-pmf.py
+++ bernoulli-pmf/bernoulli-pmf.py
@@ -5,11 +5,7 @@ def bernoulli_pmf_and_moments(x, p):
     Compute Bernoulli PMF and distribution moments.
     """
     # Write code here
-    pmf = []
-    for e in x:
-        if e == 0:
-            pmf.append(1-p)
-        else:
-            pmf.append(p)
+    x = np.array(x)
+    pmf = (p ** x) * ((1-p) ** (1-x))
 
-    return np.array(pmf), p, p*(1-p)
+    return pmf, p, p*(1-p)
